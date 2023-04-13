a8e29c9d5f05ec95eeceaf2463801aa735f7df61
Joshua Son <joshuason@umail.ucsb.edu>
2023-04-12 19:25:07 -0700

    fixed error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,10 +10,10 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
 
   return (
-    <BrowserRouter basename="/STARTER-team00">
+    <BrowserRouter basename="team00-s23-7pm-4">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/towns/Seattle" element={<Seattle />} />
+        <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
         <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
       </Routes>
