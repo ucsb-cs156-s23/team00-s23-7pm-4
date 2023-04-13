1dcf95386ece7d17c91788f3a5f4209b9d4f333e
Joshua Son <joshuason@umail.ucsb.edu>
2023-04-12 19:40:00 -0700

    js - added route in App.js for Seattle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import HomePage from "main/pages/HomePage";
 import AvilaBeachPage from "main/pages/AvilaBeachPage";
 import LosAlamosPage from "main/pages/LosAlamosPage";
 import ArroyoGrandePage from "main/pages/ArroyoGrandePage";
+import SeattlePage from "main/pages/SeattlePage";
+
+
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -16,6 +19,7 @@ function App() {
         <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
         <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
+        <Route exact path="/towns/Seattle" element={<SeattlePage />} />
       </Routes>
     </BrowserRouter>
   );
