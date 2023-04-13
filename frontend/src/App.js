314497899d9a68b60370cef27ae6e9a1f3fd93aa
Joshuason55 <86702859+Joshuason55@users.noreply.github.com>
2023-04-12 19:27:33 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter basename="team00-s23-7pm-4">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/towns/Seattle" element={<Seattle />} />
+        <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
         <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
       </Routes>
