105d2f01f4b08688f4ce07fa5b77eef0f90e3766
ethan nguyen <ethannguyen@umail.ucsb.edu>
2023-04-12 19:50:06 -0700

    en - add page for Irvine

---
+++ frontend/src/main/pages/IrvinePage.js
@@ -0,0 +1,19 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function IrvinePage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Irvine, CA</h1>
+        <p>
+         <b>Irvine, California</b> is a city in Orange County, California.
+         Irvine is well known for its safe environment as it boasts some of the 
+         lowest crime rate in the country.It features locations such as the Irvine 
+         Spectrum, a fantastic outdoor mall, and the Great Park, the Central Park of 
+         the west coast. Additionally, Irvine has some of the best food around. 
+        </p>
+        <p>Added by Ethan Nguyen</p>
+      </div>
+    </BasicLayout>
+  )
+}
