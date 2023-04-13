033d13f6c5a5a511149525bf2a5b2cd45427ee57
Brenna <brennascholte@ucsb.edu>
2023-04-12 19:53:19 -0700

    bs - add Oceanside page to nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "main/pages/HomePage";
 import AvilaBeachPage from "main/pages/AvilaBeachPage";
 import LosAlamosPage from "main/pages/LosAlamosPage";
 import ArroyoGrandePage from "main/pages/ArroyoGrandePage";
+import OceansidePage from "main/pages/OceansidePage";
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
         <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
+        <Route exact path="/towns/Oceanside" element={<OceansidePage />} />
       </Routes>
     </BrowserRouter>
   );
