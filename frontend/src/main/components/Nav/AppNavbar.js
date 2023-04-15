de232c8cac61e87df300e792bc1bd50672547447
ethan nguyen <ethannguyen@umail.ucsb.edu>
2023-04-15 01:02:43 -0700

    en - added page in AppNavbar.js for Irvine

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -23,6 +23,7 @@ export default function AppNavbar() {
                 <NavDropdown.Item as={Link} to="/towns/ArroyoGrande">Arroyo Grande</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/towns/AvilaBeach">Avila Beach</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/towns/LosAlamos">Los Alamos</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/towns/Irvine">Irvine</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
